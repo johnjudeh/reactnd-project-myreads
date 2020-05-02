8323acb2378be168337787f8dc9f990b46666498
John Judeh <johnjudeh@hotmail.com>
2020-05-02 23:44:27 +0100

    feat: Get books from BooksAPI on BooksApp mount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-// import * as BooksAPI from './BooksAPI';
+import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
 import BookLibrary from './BookLibrary';
 import BookSearch from './BookSearch';
 import './App.css';
 
 class BooksApp extends Component {
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then(books => this.setState({
+                books: books
+            }));
+    }
 
     renderBookLibrary() {
         return <BookLibrary />;
