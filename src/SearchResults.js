9e8a686c85a60dfb9ce33a23ae7b84b105774d91
John Judeh <johnjudeh@hotmail.com>
2020-05-03 15:04:08 +0100

    feat: Break BookSearch up into smaller components

---
+++ src/SearchResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchResults(props) {
+    return (
+        <div className="search-books-results">
+            <ol className="books-grid"></ol>
+        </div>
+    );
+}
+
+export default SearchResults;
