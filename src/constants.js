b1f9ab333dc4b6665046f9bdc6277f5d8c1d7204
John Judeh <johnjudeh@hotmail.com>
2020-05-03 21:44:55 +0100

    Style: Add missing semicolons to the constants module

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
-export const BOOKSHELF_VAL_CURRENTLY_READING = 'currentlyReading'
-export const BOOKSHELF_VAL_WANT_TO_READ = 'wantToRead'
-export const BOOKSHELF_VAL_READ = 'read'
-export const BOOKSHELF_VAL_NONE = 'none'
+export const BOOKSHELF_VAL_CURRENTLY_READING = 'currentlyReading';
+export const BOOKSHELF_VAL_WANT_TO_READ = 'wantToRead';
+export const BOOKSHELF_VAL_READ = 'read';
+export const BOOKSHELF_VAL_NONE = 'none';
 
 export const BOOKSHELF_LABELS = {
     [BOOKSHELF_VAL_CURRENTLY_READING]: "Currently Reading",
@@ -25,14 +25,14 @@ const PROPTYPE_SHAPE_BASE_BOOK = {
     }),
     title: PropTypes.string.isRequired,
     authors: PropTypes.arrayOf(PropTypes.string),
-}
+};
 
 export const PROPTYPE_SHAPE_BOOK = {
     ...PROPTYPE_SHAPE_BASE_BOOK,
     shelf: PropTypes.oneOf(PERMENANT_BOOKSHELVES).isRequired,
-}
+};
 
 export const PROPTYPE_SHAPE_BOOK_ALL_SHELVES = {
     ...PROPTYPE_SHAPE_BASE_BOOK,
     shelf: PropTypes.oneOf(Object.keys(BOOKSHELF_LABELS)).isRequired
-}
+};
